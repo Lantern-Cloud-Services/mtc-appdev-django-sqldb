b4a2a275c8a38ddf08629d3f962628727efea9b7
jdvcDev <jason.cook@microsoft.com>
2021-04-07 15:41:02 -0600

    remove SQL for initial debug

--- djangodbapp/views.py
+++ djangodbapp/views.py
@@ -14,14 +14,14 @@ def index(request):
 
     result = "<h1>Hello from django</h1><br /><br /><b>DB Stuff:</b><br />"
 
-    with pyodbc.connect(
-            'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
-        with conn.cursor() as cursor:
-            cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-            row = cursor.fetchone()
-            while row:
-                result += str(row[0]) + " " + str(row[1] + "<br />")
-                print(str(row[0]) + " " + str(row[1]))
-                row = cursor.fetchone()
+#    with pyodbc.connect(
+#            'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
+#        with conn.cursor() as cursor:
+#            cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+#            row = cursor.fetchone()
+#            while row:
+#                result += str(row[0]) + " " + str(row[1] + "<br />")
+#                print(str(row[0]) + " " + str(row[1]))
+#                row = cursor.fetchone()
 
     return HttpResponse(result)
