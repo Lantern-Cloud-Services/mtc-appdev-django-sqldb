830a8d74768339b84e77ab872a90e2602032b564
jdvcDev <jason.cook@microsoft.com>
2021-04-07 16:12:15 -0600

    abstract connection string

--- djangodbapp/views.py
+++ djangodbapp/views.py
@@ -1,3 +1,4 @@
+import os
 from django.shortcuts import render
 from django.http import HttpResponse
 import pyodbc
@@ -5,23 +6,45 @@ import pyodbc
 # Create your views here.
 def index(request):
     # TODO move app property
-    server = 'demo-mtc-appdev-sqlser1.database.windows.net'
-    database = 'demo-mtc-appdev-sqldb1'
-    username = ''
-    password = ''
-    driver = '{ODBC Driver 17 for SQL Server}'
+    #server = 'demo-mtc-appdev-sqlser1.database.windows.net'
+    #database = 'demo-mtc-appdev-sqldb1'
+    #username = ''
+    #password = ''
+    #driver = '{ODBC Driver 17 for SQL Server}'
+
+
+
 
 
     result = "<h1>Hello from django</h1><br /><br /><b>DB Stuff:</b><br />"
 
+    sqlstr = os.environ.get('SQLCONNSTR_dbconststr', "SQLCONNSTR_dbconststr variable does not exist")
+#    sqlstrarr = sqlstr.split(";")
+
+#    sqlstrmap = {}
+#    for string in sqlstrarr:
+#        if string is not '':
+#            key = string.split('=')[0]
+#            val = string.split('=')[1]
+#            sqlstrmap[key] = val
+
+#    server = sqlstrmap['Server']
+#    database = sqlstrmap['Initial Catalog']
+#    username = sqlstrmap['User ID']
+#    password = sqlstrmap['Password']
+
+    #return 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + dbusername + ';PWD=' + dbpassword
+
 #    with pyodbc.connect(
 #            'DRIVER=' + driver + ';SERVER=' + server + ';PORT=1433;DATABASE=' + database + ';UID=' + username + ';PWD=' + password) as conn:
-#        with conn.cursor() as cursor:
-#            cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-#            row = cursor.fetchone()
-#            while row:
-#                result += str(row[0]) + " " + str(row[1] + "<br />")
-#                print(str(row[0]) + " " + str(row[1]))
-#                row = cursor.fetchone()
+
+    with pyodbc.connect(sqlstr) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+            row = cursor.fetchone()
+            while row:
+                result += str(row[0]) + " " + str(row[1] + "<br />")
+                print(str(row[0]) + " " + str(row[1]))
+                row = cursor.fetchone()
 
     return HttpResponse(result)
